e395ebba59eb81f3a62c2ca292342199a6aefa96
KishenKumarrrrr <kishen1198@gmail.com>
2020-05-11 12:34:36 +0800

    Landing Page Created

---
+++ src/components/index.js
@@ -0,0 +1,4 @@
+export { default as Header} from './Header/Header';
+export { default as IntroBanner} from './IntroBanner/IntroBanner';
+export { default as SecondBanner} from './SecondBanner/SecondBanner';
+export { default as Chart} from './Chart/Chart';
