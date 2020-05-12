c932b7ed8a03529eb5593a77cf74eda99a1de21b
KishenKumarrrrr <kishen1198@gmail.com>
2020-05-12 11:03:15 +0800

    Remove duplicated directory

--- src/components/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Header} from './Header/Header';
-export { default as IntroBanner} from './IntroBanner/IntroBanner';
-export { default as SecondBanner} from './SecondBanner/SecondBanner';
-export { default as Chart} from './Chart/Chart';
