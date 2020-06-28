162adb4f5efead43e2fac6fba6e693c52c752bef
KishenKumarrrrr <kishen1198@gmail.com>
2020-06-28 19:19:06 +0800

    again

--- server.js
+++ server.js
@@ -27,13 +27,12 @@ const cardRouter = require('./routes/cards');
 app.use('/decks', deckRouter);
 app.use('/cards', cardRouter);
 
-if (process.env.NODE_ENV == "production") {
-    app.use(express.static('client/build'));
+app.use(express.static('client/build'));
 
-    app.get("*", (req, res) => {
-        res.sendFile(path.join(__dirname, "client", "build", "index.html"));
-    });
-}
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+});
+
 
 app.use('/', express.static(path.join(__dirname, 'public')))
 
